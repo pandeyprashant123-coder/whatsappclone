97ef1b12199e633279e9dafcbc92d0e9208a1c88
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2024-04-26 09:26:15 +0545

    express-app setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,28 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
+import express, {Request, Response} from "express";
 
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
+app.get("/", (req:Request, res:Response) => {
+  res.send("Server is running");
+});
 
 io.on("connection", (socket) => {
   try {
-    const id = socket.handshake.query.id;
+    const id = socket.handshake.query.id!;
     // console.log(id, "id");
     socket.join(id);
 
     socket.on("send-message", ({ recipients, text }) => {
-      recipients.forEach((recipient) => {
-        const newRecipients = recipients.filter((r) => r !== recipient);
+      recipients.forEach((recipient:string) => {
+        const newRecipients = recipients.filter((r:string) => r !== recipient);
         newRecipients.push(id);
         socket.broadcast.to(recipient).emit("receive-message", {
           recipients: newRecipients,
